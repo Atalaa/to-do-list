dba6ebcef89139a3b63ae89e17aeaefa88f26907
Atala <atala.rchidi@gmail.com>
2020-10-14 11:51:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (e) => {
+    const newVvalue = e.target.value;
+    setTask(newVvalue);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (task === "") {
+      return;
+    }
+    setTaskList((prevTask) => {
+      return [...prevTask, task];
+    });
+
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const arrList = [...taskList];
+    arrList.splice(index, 1);
+    console.log(arrList);
+
+    setTaskList(arrList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <form>
+          <input name="item" value={task} type="text" onChange={handleChange} />
+          <button onClick={handleClick}>
+            <span>Add</span>
+          </button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {taskList.map((item, index) => (
+            <li key={item}>
+              {item}{" "}
+              <span className="close" onClick={() => deleteTask(index)}>
+                X
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
