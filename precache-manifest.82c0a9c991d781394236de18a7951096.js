7a05eac7842f16f8a47d59bbf6831c0f264d9d27
Atala <atala.rchidi@gmail.com>
2020-10-14 12:02:47 -0400

    Updates

---
+++ precache-manifest.82c0a9c991d781394236de18a7951096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9910229d47c94437d8d8b8d9c5b6a2",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "4a6c6ff8265930cd2536",
+    "url": "/to-do-list/static/css/main.8937d856.chunk.css"
+  },
+  {
+    "revision": "10e70d2828e7611b6538",
+    "url": "/to-do-list/static/js/2.fb4bf519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.fb4bf519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6c6ff8265930cd2536",
+    "url": "/to-do-list/static/js/main.f17c98fc.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
